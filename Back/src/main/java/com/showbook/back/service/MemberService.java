ef6d8f5e988a420c743993b7c4ad17421a3795c7
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-03-11 13:46:16 +0900

    :sparkles: Feat: 패키지 구조 설정

---
+++ Back/src/main/java/com/showbook/back/service/MemberService.java
@@ -0,0 +1,10 @@
+package com.showbook.back.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+}
