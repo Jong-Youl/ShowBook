11ab2671d4b0d35f24922d2666663a4f105acf84
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-03-12 09:02:49 +0900

    :sparkles: Feat: JwtAuthenticationEntryPoint추가 및 JwtTokenUtil 수정

--- Back/src/main/java/com/showbook/back/security/jwt/JwtTokenUtil.java
+++ Back/src/main/java/com/showbook/back/security/jwt/JwtTokenUtil.java
@@ -3,6 +3,7 @@ package com.showbook.back.security.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,7 +21,7 @@ public class JwtTokenUtil {
     private final Integer REFRESH_EXPIRATION_TIME;
 
     public static final String PREFIX = "Bearer "; // 띄어쓰기 있어야 한다
-    private static final String ISSUER = "com.test.security";
+    private static final String ISSUER = "shook";
     public static final String HEADER_STRING = "Authorization";
 
     public JwtTokenUtil(@Value("${ACCESS_SECRET_KEY}") String ACCESS_SECRET_KEY,
@@ -33,18 +34,20 @@ public class JwtTokenUtil {
         this.REFRESH_EXPIRATION_TIME = REFRESH_EXPIRATION_TIME;
     }
 
-    public String createAccessToken(String id) {
+    public String createAccessToken(String id, String email, String role) {
         String accessToken = JWT.create()
                 .withSubject("accessToken") // 토큰 제목
                 .withExpiresAt(new Date(System.currentTimeMillis() + ACCESS_EXPIRATION_TIME)) // access Token 만료시간
                 .withIssuer(ISSUER) // 발급자
                 .withClaim("id",id)
+                .withClaim("email",email)
+                .withClaim("role",role)
                 .sign(Algorithm.HMAC256(ACCESS_SECRET_KEY));
 
         return PREFIX + accessToken;
     }
 
-    public String createRefreshToken(Long id) {
+    public String createRefreshToken() {
         String refreshToken =JWT.create()
                 .withSubject("refreshToken")
                 .withExpiresAt(new Date(System.currentTimeMillis() + REFRESH_EXPIRATION_TIME))
@@ -74,8 +77,19 @@ public class JwtTokenUtil {
         }
     }
 
+    public boolean isTokenValidated(String jwtToken) {
+        try {
+            DecodedJWT decodedJWT = verify(jwtToken, ACCESS_SECRET_KEY);
+            return decodedJWT != null && !decodedJWT.getExpiresAt().before(new Date());
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+
     private DecodedJWT verify(String token, String key) {
         try {
+            // HMAC256 알고리즘을 사용하여 JWT를 확인하기 위한 JWTVerifier를 생성
             JWTVerifier jwtVerifier =JWT.require(Algorithm.HMAC256(key)).withIssuer(ISSUER).build();
             return jwtVerifier.verify(token);
         } catch (Exception e) {
