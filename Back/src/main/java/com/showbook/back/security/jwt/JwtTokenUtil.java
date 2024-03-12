51ee0ebe697f5a3799e1653dc65ce68cce715fd5
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-03-12 14:15:06 +0900

    :sparkles: Feat: jwt 관련 설정 추가

--- Back/src/main/java/com/showbook/back/security/jwt/JwtTokenUtil.java
+++ Back/src/main/java/com/showbook/back/security/jwt/JwtTokenUtil.java
@@ -5,18 +5,22 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.showbook.back.dto.RefreshToken;
+import com.showbook.back.repository.RefreshTokenRepository;
+import com.showbook.back.security.dto.GeneratedToken;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class JwtTokenUtil {
 
-    private final String ACCESS_SECRET_KEY;
-    private final String REFRESH_SECRET_KEY;
+    private final String SECRET_KEY;
     private final Integer ACCESS_EXPIRATION_TIME;
     private final Integer REFRESH_EXPIRATION_TIME;
 
@@ -24,42 +28,52 @@ public class JwtTokenUtil {
     private static final String ISSUER = "shook";
     public static final String HEADER_STRING = "Authorization";
 
-    public JwtTokenUtil(@Value("${ACCESS_SECRET_KEY}") String ACCESS_SECRET_KEY,
-                        @Value("${REFRESH_SECRET_KEY}") String REFRESH_SECRET_KEY,
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public JwtTokenUtil(@Value("${SECRET_KEY}") String SECRET_KEY,
                         @Value("${ACCESS_EXPIRATION_TIME}")Integer ACCESS_EXPIRATION_TIME,
-                        @Value("${REFRESH_EXPIRATION_TIME}")Integer REFRESH_EXPIRATION_TIME){
-        this.ACCESS_SECRET_KEY = ACCESS_SECRET_KEY;
+                        @Value("${REFRESH_EXPIRATION_TIME}")Integer REFRESH_EXPIRATION_TIME,
+                        RefreshTokenRepository refreshTokenRepository){
+        this.SECRET_KEY = SECRET_KEY;
         this.ACCESS_EXPIRATION_TIME = ACCESS_EXPIRATION_TIME;
-        this.REFRESH_SECRET_KEY = REFRESH_SECRET_KEY;
         this.REFRESH_EXPIRATION_TIME = REFRESH_EXPIRATION_TIME;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
-    public String createAccessToken(String id, String email, String role) {
+    public GeneratedToken generateTokens(String email, String role) {
+        String accessToken = createAccessToken(email, role);
+        String refreshToken = createRefreshToken(email, role);
+
+        return  new GeneratedToken(accessToken,refreshToken);
+    }
+
+    public String createAccessToken(String email, String role) {
         String accessToken = JWT.create()
                 .withSubject("accessToken") // 토큰 제목
                 .withExpiresAt(new Date(System.currentTimeMillis() + ACCESS_EXPIRATION_TIME)) // access Token 만료시간
                 .withIssuer(ISSUER) // 발급자
-                .withClaim("id",id)
                 .withClaim("email",email)
                 .withClaim("role",role)
-                .sign(Algorithm.HMAC256(ACCESS_SECRET_KEY));
+                .sign(Algorithm.HMAC256(SECRET_KEY));
 
         return PREFIX + accessToken;
     }
 
-    public String createRefreshToken() {
+    public String createRefreshToken(String email, String role) {
         String refreshToken =JWT.create()
                 .withSubject("refreshToken")
                 .withExpiresAt(new Date(System.currentTimeMillis() + REFRESH_EXPIRATION_TIME))
                 .withIssuer(ISSUER)
-                .sign(Algorithm.HMAC256(REFRESH_SECRET_KEY));
+                .withClaim("email",email)
+                .withClaim("role",role)
+                .sign(Algorithm.HMAC256(SECRET_KEY));
 
         return PREFIX + refreshToken;
     }
 
     public String checkAccessToken(String token) {
         token = token.replace(PREFIX, "");
-        DecodedJWT decodedJWT = verify(token, ACCESS_SECRET_KEY);
+        DecodedJWT decodedJWT = verify(token);
         if (decodedJWT != null && decodedJWT.getSubject().equals("accessToken")) {
             return decodedJWT.getClaim("id").asString();
         } else {
@@ -69,7 +83,7 @@ public class JwtTokenUtil {
 
     public boolean checkRefreshToken(String token) {
         token = token.replace(PREFIX, "");
-        DecodedJWT decodedJWT = verify(token, REFRESH_SECRET_KEY);
+        DecodedJWT decodedJWT = verify(token);
         if (decodedJWT != null && decodedJWT.getSubject().equals("refreshToken")) {
             return true;
         } else {
@@ -77,9 +91,19 @@ public class JwtTokenUtil {
         }
     }
 
+    public boolean existsRefreshToken(String refreshToken) {
+
+        Optional<RefreshToken> token = refreshTokenRepository.findById(refreshToken);
+
+        if (token != null) return true;
+
+        return false;
+
+    }
+
     public boolean isTokenValidated(String jwtToken) {
         try {
-            DecodedJWT decodedJWT = verify(jwtToken, ACCESS_SECRET_KEY);
+            DecodedJWT decodedJWT = verify(jwtToken);
             return decodedJWT != null && !decodedJWT.getExpiresAt().before(new Date());
         } catch (JWTVerificationException e) {
             return false;
@@ -87,15 +111,39 @@ public class JwtTokenUtil {
     }
 
 
-    private DecodedJWT verify(String token, String key) {
+    public DecodedJWT verify(String token) {
         try {
             // HMAC256 알고리즘을 사용하여 JWT를 확인하기 위한 JWTVerifier를 생성
-            JWTVerifier jwtVerifier =JWT.require(Algorithm.HMAC256(key)).withIssuer(ISSUER).build();
+            JWTVerifier jwtVerifier =JWT.require(Algorithm.HMAC256(SECRET_KEY)).withIssuer(ISSUER).build();
             return jwtVerifier.verify(token);
         } catch (Exception e) {
             log.error(e.getMessage());
             return null;
         }
     }
+
+    public String getEmail(String token) {
+        token = token.replace(PREFIX,"");
+        DecodedJWT decodedJWT = verify(token);
+
+        if (decodedJWT != null) {
+            return decodedJWT.getClaim("email").asString();
+        } else {
+            return null;
+        }
+    }
+
+    public String getRole(String token) {
+        token = token.replace(PREFIX,"");
+        DecodedJWT decodedJWT = verify(token);
+
+        if (decodedJWT != null) {
+            return decodedJWT.getClaim("email").asString();
+        } else {
+            return null;
+        }
+    }
+
+
 
 }
