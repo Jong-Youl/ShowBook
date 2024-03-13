cfc9528464a07ddb9f0a5371b2aff51737f1d0c3
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-03-13 17:11:33 +0900

    :sparkles: Feat: oAuth2 회원가입 후 추가 정보 입력 로직

--- Back/src/main/java/com/showbook/back/entity/Member.java
+++ Back/src/main/java/com/showbook/back/entity/Member.java
@@ -1,6 +1,7 @@
 package com.showbook.back.entity;
 
 
+import com.showbook.back.dto.request.SignupRequestDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
