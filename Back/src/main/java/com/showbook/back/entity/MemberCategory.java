e20169e53807530941b1fcbe352f314fe0885dc8
허준호 <nidolight@icloud.com>
2024-03-14 09:05:35 +0900

    Revert "Merge branch 'develop/front' into 'main'"
    This reverts merge request !9

---
+++ Back/src/main/java/com/showbook/back/entity/MemberCategory.java
@@ -0,0 +1,25 @@
+package com.showbook.back.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class MemberCategory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberCategoryId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@Enumerated(EnumType.STRING)
+	private Category category;
+}
---
+++ Back/src/main/java/com/showbook/back/entity/Shook.java
@@ -0,0 +1,36 @@
+package com.showbook.back.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Shook {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long shookId;
+
+	@Column(nullable = false)
+	private String shookImageUrl;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="book_id")
+	private Book book;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="member_id")
+	private Member member;
+
+
+
+}
