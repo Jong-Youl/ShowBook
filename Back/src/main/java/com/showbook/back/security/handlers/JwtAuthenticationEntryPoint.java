6e86df6bbca937f5b4544ba41827b4b217f3dda5
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-03-18 08:08:41 +0900

    :zap: Update: jwt 토큰 관련 코드 수정

--- Back/src/main/java/com/showbook/back/security/handlers/JwtAuthenticationEntryPoint.java
+++ Back/src/main/java/com/showbook/back/security/handlers/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package com.showbook.back.security.handlers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,30 +10,17 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private final ObjectMapper objectMapper;
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         log.info("JwtAuthenticationEntryPoint - commence");
-//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,authException.getMessage());
-
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
-
-        Map<String, Object> responseData = new HashMap<>();
-        responseData.put("error", "Unauthorized");
-        responseData.put("message", authException.getMessage());
-
-        PrintWriter writer = response.getWriter();
-        objectMapper.writeValue(writer, responseData);
-        writer.flush();
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,authException.getMessage());
     }
 
 }
