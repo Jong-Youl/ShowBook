5ea836c2f896749e4011874075354cae547192d1
허준호 <nidolight@icloud.com>
2024-03-18 17:41:56 +0900

    :Sparkles: Feat: shorts form added

--- front/src/pages/Shorts/index.jsx
+++ front/src/pages/Shorts/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import HTMLFlipBook from 'react-pageflip';
+import ShortForm from '../../components/ShortForm';
 
 const Shorts = () => {
   return (
-    <HTMLFlipBook width={400} height={600}>
-      <div className='demoPage'>Page 1</div>
-      <div className='demoPage'>Page 2</div>
-      <div className='demoPage'>Page 3</div>
-      <div className='demoPage'>Page 4</div>
-    </HTMLFlipBook>
+<div>
+<ShortForm></ShortForm>
+</div>
   );
 };
 
