34f4a853b9fe04b079b942f1005e19883e3057ac
허준호 <nidolight@icloud.com>
2024-03-20 17:28:46 +0900

    :sparkles: Feat: library Navbar component added

--- front/src/pages/Library/index.jsx
+++ front/src/pages/Library/index.jsx
@@ -1,17 +1,15 @@
 // Library.jsx
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import BeforeRead from './BeforeRead';
 import NowRead from './NowRead';
 import AfterRead from './AfterRead';
+import Navbar from './Navbar';
 
 function Library() {
   return (
     <div>
       <h2>서재</h2>
-      <nav>
-        <Link to='before'>읽고 싶은 책</Link> |{' '}
-        <Link to='now'>읽고 있는 책</Link> | <Link to='after'>읽은 책</Link>
-      </nav>
+      <Navbar></Navbar>
       <Routes>
         <Route path='before' element={<BeforeRead />} />
         <Route path='now' element={<NowRead />} />
