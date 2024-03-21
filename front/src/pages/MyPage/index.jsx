ef9949508a906dbf9624f2669e36921b5a1df953
허준호 <nidolight@icloud.com>
2024-03-21 17:44:41 +0900

    :sparkle: Feat: my Page added

--- front/src/pages/MyPage/index.jsx
+++ front/src/pages/MyPage/index.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from 'react';
+import {
+  Container,
+  Content,
+  EditLink,
+  Nickname,
+  ProfileHeader,
+  ProfileImage,
+  ToggleButton,
+  ToggleButtonContainer,
+} from './MyPage.styles';
 
 const MyPage = () => {
-    return <div>my Page</div>;
-}
+  const [activeTab, setActiveTab] = useState('reviews');
+
+  return (
+    <Container>
+      <ProfileHeader>
+        <ProfileImage src='img/1.png' alt='Profile' />
+        <div>
+          <Nickname>조용한 가재</Nickname>
+          <EditLink href='/edit-profile'>개인정보 수정</EditLink>
+        </div>
+      </ProfileHeader>
+      <ToggleButtonContainer>
+        <ToggleButton
+          active={activeTab === 'reviews'}
+          onClick={() => setActiveTab('reviews')}
+        >
+          나의 한줄평
+        </ToggleButton>
+        <ToggleButton
+          active={activeTab === 'records'}
+          onClick={() => setActiveTab('records')}
+        >
+          나의 기록
+        </ToggleButton>
+      </ToggleButtonContainer>
+      <Content>
+        {activeTab === 'reviews' ? (
+          <div>나의 한줄평입니다.</div>
+        ) : (
+          <div>나의 기록입니다.</div>
+        )}
+      </Content>
+    </Container>
+  );
+};
 
 export default MyPage;
