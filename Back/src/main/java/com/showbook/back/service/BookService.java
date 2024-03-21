e4e5e3898e1ef7f4c718a4c803dc84ffe53e3f1d
임종율 <dlawhdfbf12@naver.com>
2024-03-21 14:10:34 +0900

    :sparkles: Feat: 책 구매 페이지 반환 API 테스트 완료

--- Back/src/main/java/com/showbook/back/service/BookService.java
+++ Back/src/main/java/com/showbook/back/service/BookService.java
@@ -1,14 +1,12 @@
 package com.showbook.back.service;
 
-import static com.showbook.back.common.constants.ErrorCode.*;
-
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.showbook.back.common.constants.ErrorCode;
-import com.showbook.back.common.exception.CustomException;
 import com.showbook.back.dto.response.BookDetailResponseDTO;
+import com.showbook.back.dto.response.BookPurchaseResponseDTO;
 import com.showbook.back.entity.Book;
 import com.showbook.back.execption.BookNotFoundException;
 import com.showbook.back.repository.BookRepository;
@@ -21,6 +19,13 @@ import lombok.extern.log4j.Log4j2;
 @Service
 public class BookService {
 
+	@Value("${purchase.prefix}")
+	private String prefix;
+	@Value("${purchase.suffix}")
+	private String suffix;
+	@Value("${purchase.bridge}")
+	private String bridge;
+
 	private final BookRepository bookRepository;
 
 	public BookDetailResponseDTO getDetail(long bookId) {
@@ -30,5 +35,32 @@ public class BookService {
 		return book.map(value -> BookDetailResponseDTO.builder()
 			.book(value)
 			.build()).orElseThrow(BookNotFoundException::new);
+	}
+
+	public BookPurchaseResponseDTO getPurchaseUrl(long bookId) {
+
+		Book book = bookRepository.findById(bookId).
+			orElseThrow(BookNotFoundException::new);
+
+		return BookPurchaseResponseDTO.builder()
+			.url(transformTitleToUrl(book))
+			.build();
+	}
+
+	public String transformTitleToUrl(Book book) {
+		StringBuilder result = new StringBuilder();
+		String target1 = book.getTitle();
+		String target2 = book.getPublisher();
+
+		result.append(prefix);
+
+		for (int i = 0; i < target1.length(); i++) {
+			char tmp = target1.charAt(i);
+			if (tmp == ' ') result.append(bridge);
+			else result.append(tmp);
+		}
+
+		result.append(bridge).append(target2).append(suffix);
+		return result.toString();
 	}
 }
