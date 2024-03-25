ae15fd98ff6e233503bebb2f6ce97d16c14c777e
주윤 <joo6881@naver.com>
2024-03-25 17:42:21 +0900

    :bug: Fix: icon 버그 수정

---
+++ front/src/etc/reviewsJson.js
@@ -0,0 +1,131 @@
+// private final String title;
+// private final String content;
+// private final LocalDate createdAt;
+// private final LocalDate updatedAt;
+// private final int rating;
+// private final String nickname;
+// private final String memberImageUrl;
+
+
+export const reviewsJson = [
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+    {
+        content : "리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰리뷰",
+        createdAt : "2024-03-25",
+        rating : "4.5",
+        nickname : "nick1",
+        memberImageUrl : "https://showbookbucket.s3.ap-northeast-2.amazonaws.com/user-imgage/%EA%B8%B0%EB%B3%B8+%EC%9C%A0%EC%A0%80+%EC%9D%B4%EB%AF%B8%EC%A7%80.png"
+    },
+
+]
