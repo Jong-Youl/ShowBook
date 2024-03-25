d150c5e8b91fe78c92db025ef3d81a169002540e
허준호 <nidolight@icloud.com>
2024-03-25 17:07:38 +0900

    :sparkles: Feat: MyReview/MyHistory Page added

---
+++ front/src/pages/MyPage/MyReview/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  ChartContainer,
+  ChartTitle,
+} from '../../../components/common/styles/CommonStyles';
+import ReadBookMonthlyChart from '../../../components/ReadBookMonthlyChart';
+
+const MyReview = () => {
+  const bookData = [
+    { month: 'Jan', count: 9 },
+    { month: 'Feb', count: 0 },
+    { month: 'Mar', count: 1 },
+    { month: 'Apr', count: 1 },
+    { month: 'May', count: 5 },
+    { month: 'Jun', count: 4 },
+    { month: 'Jul', count: 1 },
+    { month: 'Aug', count: 1 },
+    { month: 'Sep', count: 6 },
+    { month: 'Oct', count: 7 },
+    { month: 'Nov', count: 1 },
+    { month: 'Dec', count: 1 },
+  ];
+
+  return (
+    <>
+      <ChartTitle>월별 독서량</ChartTitle>
+      <ChartContainer>
+        <ReadBookMonthlyChart bookData={bookData} />
+      </ChartContainer>
+    </>
+  );
+};
+
+export default MyReview;
