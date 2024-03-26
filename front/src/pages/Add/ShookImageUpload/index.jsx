216534ffbb12ec90baec905ee6d95c5c951964b6
허준호 <nidolight@icloud.com>
2024-03-26 11:03:37 +0900

    :sparkles: Feat: book-selection/image-selection 기능 구현

--- front/src/pages/Add/ShookImageUpload/index.jsx
+++ front/src/pages/Add/ShookImageUpload/index.jsx
@@ -1,28 +1,47 @@
 import React, { useState } from 'react';
 
-const ShookCreationPage = () => {
+const ShookImageUpload = () => {
   const [image, setImage] = useState(null);
+  const [imagePreviewUrl, setImagePreviewUrl] = useState('');
 
   const handleImageChange = (event) => {
-    setImage(event.target.files[0]);
+    const file = event.target.files[0];
+    if (file) {
+      setImage(file);
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreviewUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
   };
 
   const handleSubmit = async () => {
     if (!image) {
       alert('이미지를 업로드해주세요.');
-      return;
     }
 
-    // 이미지와 함께 API 요청 보내기 로직
-    // 예: formData를 사용하여 POST 요청
+    // 이미지와 함께 API 요청 로직
   };
 
   return (
     <div>
-      <input type='file' onChange={handleImageChange} />
+      <input
+        type='file'
+        accept='image/png, image/jpeg'
+        onChange={handleImageChange}
+      />
       <button onClick={handleSubmit}>작성</button>
+      {imagePreviewUrl && (
+        <img
+          src={imagePreviewUrl}
+          alt='preview'
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
+      )}
     </div>
   );
 };
 
-export default ShookCreationPage;
+export default ShookImageUpload;
