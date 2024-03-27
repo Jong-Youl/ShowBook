3e538da843517cc4021f514754ce1be47a145756
허준호 <nidolight@icloud.com>
2024-03-27 17:34:11 +0900

    :buf: Fix: recoil bug fixed

--- front/src/pages/User/Proxy/index.jsx
+++ front/src/pages/User/Proxy/index.jsx
@@ -1,37 +1,36 @@
-import {useState,useEffect} from 'react';
-import { memberState } from '../../../recoil/memberRecoil';
+import { useState, useEffect } from 'react';
+import { memberState } from '../../../lib/memberRecoil';
 import { useSetRecoilState } from 'recoil';
 import { useNavigate } from 'react-router';
-import {MemberService} from '../../../api/MemberService';
+import { MemberService } from '../../../api/MemberService';
 
 const memberService = new MemberService();
 
 function Proxy() {
-    const [memberId, setMemberId] = useState(null); 
-    const setMemberInfo = useSetRecoilState(memberState)
+  const [memberId, setMemberId] = useState(null);
+  const setMemberInfo = useSetRecoilState(memberState);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const memberIdFromURL = new URL(document.location).searchParams.get("id")
-        setMemberId(memberIdFromURL)
-    },[])
+  useEffect(() => {
+    const memberIdFromURL = new URL(document.location).searchParams.get('id');
+    setMemberId(memberIdFromURL);
+  }, []);
 
-    // memberId가 변하면 실행
-    useEffect(() => {
-        const getMemberInfo = async(memberId) => {
-            let memberInfo = await memberService.login(memberId)
-            if(memberInfo){
-                setMemberInfo(memberInfo)
-            }
-        }
+  // memberId가 변하면 실행
+  useEffect(() => {
+    const getMemberInfo = async (memberId) => {
+      let memberInfo = await memberService.login(memberId);
+      if (memberInfo) {
+        setMemberInfo(memberInfo);
+      }
+    };
 
-        if(memberId){
-            getMemberInfo(memberId)
-            navigate("/main")
-        }
-    },[memberId,setMemberInfo,navigate])
-
+    if (memberId) {
+      getMemberInfo(memberId);
+      navigate('/main');
+    }
+  }, [memberId, setMemberInfo, navigate]);
 }
 
 export default Proxy;
