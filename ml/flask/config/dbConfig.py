aa55a5b5fd421b113660d10c16df9c741aa0ccc9
장승호 <jason9865@naver.com>
2024-03-27 16:20:17 +0900

    Update dbConfig.py

--- ml/flask/config/dbConfig.py
+++ ml/flask/config/dbConfig.py
@@ -2,10 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db_info = {
     'user'     : 'root',		
-    'password' : '1234',	
-    'host'     : 'localhost',	
+    'password' : 'showbook0405',	
+    'host'     : 'j10a206.p.ssafy.io',	
     'port'     : 3306,			
-    'database' : 'test'		
+    'database' : 'showbook2'		
 }
 
 DB_URL = f"mysql+mysqlconnector://{db_info['user']}:{db_info['password']}@{db_info['host']}:{db_info['port']}/{db_info['database']}?charset=utf8"
