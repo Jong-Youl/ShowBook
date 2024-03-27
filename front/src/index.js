b390e14b79f86559c07d7b7955b22d4342918650
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-03-27 11:24:32 +0900

    :sparkles: Feat: 유저정보 상태관리 기능 추가

--- front/src/index.js
+++ front/src/index.js
@@ -3,13 +3,16 @@ import './index.css';
 import App from './App';
 import GlobalFonts from './styles/fonts';
 import { createRoot } from 'react-dom/client'; // createRoot를 import
+import {RecoilRoot} from 'recoil'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <GlobalFonts />
-    <App />
-  </React.StrictMode>,
+    <RecoilRoot>
+      <GlobalFonts />
+      <App />
+    </RecoilRoot>,
+  </React.StrictMode>
 );
