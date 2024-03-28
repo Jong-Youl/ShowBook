36871854e73c135e2cad89727e5fd5f12f32483f
허준호 <nidolight@icloud.com>
2024-03-28 14:15:32 +0900

    :sparkles: Feat: shorts front status added

--- front/src/components/ShortForm/ShortForm.styles.js
+++ front/src/components/ShortForm/ShortForm.styles.js
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 export const StyledSwiperSlide = styled(SwiperSlide)`
+  transform-origin: left center;
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 12px;
   font-size: 22px;
   font-weight: bold;
-  color: #fff;
 `;
 
 export const StyledSwiper = styled(Swiper)`
@@ -19,12 +19,20 @@ export const StyledSwiper = styled(Swiper)`
 `;
 export const CurrentBookTitle = styled.div`
   position: absolute;
-  top: 0;
+  top: 20px;
   left: 50%;
   transform: translateX(-50%);
   color: #000;
   font-size: 24px;
-  background-color: #fff;
   padding: 10px;
-  border-radius: 8px;
+`;
+
+export const CurrentBookLike = styled.div`
+  position: absolute;
+  bottom: 11vh;
+  left: 50%;
+  transform: translateX(0);
+  max-width: 456px;
+  font-size: 24px;
+  padding-left: 150px;
 `;
--- front/src/lib/memberRecoil.js
+++ front/src/lib/memberRecoil.js
@@ -1,18 +1,18 @@
-import {atom} from "recoil";
-import { recoilPersist } from "recoil-persist";
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
-const {persistAtom} = recoilPersist({
-    key : "localStorage",
-    storage : localStorage
-})
+const { persistAtom } = recoilPersist({
+  key: 'localStorage',
+  storage: localStorage,
+});
 
 export const memberState = atom({
-    key : "member",
-    default : {email : '', name:''},
-    effects_UNSTABLE : [persistAtom]
-})
+  key: 'member',
+  default: { email: '', name: '' },
+  effects_UNSTABLE: [persistAtom],
+});
 
 export const loginState = atom({
-    key : "login",
-    default : localStorage.getItem("accessToken") ? true : false
-})
+  key: 'login',
+  default: localStorage.getItem('accessToken') ? true : false,
+});
