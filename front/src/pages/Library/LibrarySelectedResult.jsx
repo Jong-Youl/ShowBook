3c79122826654ca3bdb92f2291213e156012031d
최이초 <choeyicho1105@gmail.com>
2024-03-29 17:27:31 +0900

    :zap: Update: 서재에서 책 클릭시 상세페이지로 넘어가기 구현

--- front/src/pages/Library/LibrarySelectedResult.jsx
+++ front/src/pages/Library/LibrarySelectedResult.jsx
@@ -6,17 +6,31 @@ import { BookGrid, BookItem } from './Library.styles';
 import { getAllbooks } from '../../api/LibraryService';
 import CategoryChangeModal from './CategoryChangeModal';
 import { useNavigate } from 'react-router';
+import { BookService } from '../../api/bookService';
+import { fetchBookReviewRating } from "../../api/ReviewService";
 
 function LibrarySelectedResult({ isEditMode }) {
   const { category } = useParams();
   const [bookList, setBookList] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedBookId, setSelectedBookId] = useState('');
+  const bookService = new BookService();
   const navigate = useNavigate();
+  //const BASE_URL = process.env.REACT_APP_BASE_URL;
 
   const library = async (x) => {
     const books = await getAllbooks(x);
     setBookList(books);
+  };
+
+  const onHandleClick = async (book_id) => {
+    try {
+      const book = await bookService.getBookDetail(book_id);
+      const rating = await fetchBookReviewRating(book_id);
+      navigate('/book-detail', { state: { book: book, reviewRating: rating } });
+    } catch (error) {
+      console.error('Error fetching book:', error);
+    }
   };
 
   useEffect(() => {
@@ -45,7 +59,8 @@ function LibrarySelectedResult({ isEditMode }) {
       setIsModalVisible(true);
     } else {
       // 편집 모드가 아닐 때 다른 페이지로 이동
-      navigate('/some-other-page');
+      //navigate('/some-other-page');
+      onHandleClick(bookId);
     }
   };
 
