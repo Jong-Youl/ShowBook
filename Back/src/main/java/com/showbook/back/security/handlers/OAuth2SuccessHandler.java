2387f93b23dc2505f77f12294c78517ce660dbe3
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-03-29 09:09:33 +0900

    :zap: Update: OAuth2SucessHandler 수정

--- Back/src/main/java/com/showbook/back/security/handlers/OAuth2SuccessHandler.java
+++ Back/src/main/java/com/showbook/back/security/handlers/OAuth2SuccessHandler.java
@@ -60,7 +60,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         if(member != null) { // 이미 존재하는 회원
             Long memberId = member.getId();
 
-            String targetUrl = UriComponentsBuilder.fromUriString(BASE_URL + "/proxy")
+            String targetUrl = UriComponentsBuilder.fromUriString(BASE_URL + "/user/proxy")
                     .queryParam("id",memberId)
                     .build()
                     .encode(StandardCharsets.UTF_8)
