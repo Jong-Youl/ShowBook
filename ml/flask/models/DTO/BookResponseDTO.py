893f6fe83cee1021c7e04e28f228a3085b5df51a
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-03-29 13:38:42 +0900

    :zap: Update: BookResponseDTO 수정

--- ml/flask/models/DTO/BookResponseDTO.py
+++ ml/flask/models/DTO/BookResponseDTO.py
@@ -2,6 +2,7 @@
 def BookResponseDTO(book):
     response = {
         "book_id" : book.book_id,
-        "book_image_url" : book.book_imageURL
+        "book_image_url" : book.book_imageURL,
+        "title" : book.title
     }
     return response
