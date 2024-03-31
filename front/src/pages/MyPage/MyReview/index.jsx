9a3fabc22d5fd78ef11ad4b5395ae1ae3278808c
허준호 <nidolight@icloud.com>
2024-04-01 02:54:02 +0900

    :sparkles: Feat: myReview infinite scroll added

--- front/src/pages/MyPage/MyReview/index.jsx
+++ front/src/pages/MyPage/MyReview/index.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import ReviewService from '../../../api/ReviewService';
+import { Container } from './MyReview.styles';
+
+const fetchMyReviews = async ({ pageParam = 0 }) => {
+  const response = await ReviewService.getMyReviews({
+    page: pageParam,
+    size: 10,
+  });
+  return response.data;
+};
 
 const MyReview = () => {
-  return <>나의 리뷰 목록입니다.</>;
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    'myReviews',
+    fetchMyReviews,
+    {
+      getNextPageParam: (lastPage) => {
+        const nextPage = lastPage.number + 1;
+        return lastPage.totalPages === 0 ? undefined : nextPage;
+      },
+    },
+  );
+
+  // Container 컴포넌트를 참조하기 위한 ref 생성
+  const containerRef = useRef(null);
+
+  // Container의 스크롤 이벤트 핸들러
+  const handleScroll = () => {
+    // containerRef.current가 null이 아니라면 스크롤 위치 계산
+    if (containerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
+      if (scrollTop + clientHeight >= scrollHeight) {
+        fetchNextPage();
+      }
+    }
+  };
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener('scroll', handleScroll);
+      return () => container.removeEventListener('scroll', handleScroll);
+    }
+  }, [handleScroll]);
+
+  return (
+    <Container ref={containerRef}>
+      {data?.pages.map((page, i) => (
+        <div key={i}>
+          {page.content.map((review, index) => (
+            <p key={index}>
+              {review.content} <br />- <br /> 평점: {review.rating}
+            </p>
+          ))}
+        </div>
+      ))}
+      {hasNextPage && <div>리뷰 로딩 중...</div>}
+      <div>모든 리뷰를 불러왔습니다.</div>
+    </Container>
+  );
 };
 
 export default MyReview;
