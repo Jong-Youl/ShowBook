5c60e45546ea77c5c453e9b34fcafd59b2130eb7
허준호 <nidolight@icloud.com>
2024-04-02 11:13:31 +0900

    :bug: Fix: navbar selected status update fixed

--- front/src/layouts/BottomNav/index.jsx
+++ front/src/layouts/BottomNav/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { NavContainer, NavItem } from './styles';
 import iconPaths from './iconPaths';
@@ -8,7 +8,22 @@ const BottomNav = () => {
   const { selectedItem, handleSelect } = useNavigation();
   const location = useLocation();
 
-  // /user/* 경로인 경우에는 컴포넌트를 렌더링하지 않음
+  useEffect(() => {
+    const path = location.pathname.split('/')[1];
+    const itemIndexMap = {
+      'main': '0',
+      'shorts': '1',
+      'add': '2',
+      'library': '3',
+      'mypage': '4',
+    };
+
+    const selectedItemIndex = itemIndexMap[path];
+    if (selectedItemIndex !== undefined && selectedItem !== selectedItemIndex) {
+      handleSelect(selectedItemIndex);
+    }
+  }, [location, handleSelect, selectedItem]);
+
   if (location.pathname.startsWith('/user/')) {
     return null;
   }
@@ -16,10 +31,14 @@ const BottomNav = () => {
   return (
     <NavContainer>
       {Object.keys(iconPaths).map((item, index) => (
-        <NavItem key={item} to={'/' + item} onClick={() => handleSelect(item)}>
+        <NavItem
+          key={item}
+          to={'/' + item}
+          onClick={() => handleSelect(String(index))}
+        >
           <img
             src={
-              selectedItem === item
+              selectedItem === String(index)
                 ? iconPaths[item].selected
                 : iconPaths[item].unselected
             }
