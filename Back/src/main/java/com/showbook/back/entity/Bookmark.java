35b2a350da3028afa061b496254328140987929a
임종율 <dlawhdfbf12@naver.com>
2024-04-02 17:22:20 +0900

    :sparkles: Feat: 북마크 수정

---
+++ Back/src/main/java/com/showbook/back/entity/Bookmark.java
@@ -0,0 +1,36 @@
+package com.showbook.back.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Bookmark {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long bookmarkId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "book_id", nullable = false)
+	private Book book;
+
+	@Builder
+	public Bookmark(Book book, Member member) {
+		this.book = book;
+		this.member = member;
+	}
+}
