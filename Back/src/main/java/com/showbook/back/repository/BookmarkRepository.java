4a54acccaf09a7cddd4c3676a1188988ef069bc7
임종율 <dlawhdfbf12@hs.ac.kr>
2024-04-03 10:09:56 +0900

    :zap: Update: 책 북마크 기능 수정

--- Back/src/main/java/com/showbook/back/repository/BookmarkRepository.java
+++ Back/src/main/java/com/showbook/back/repository/BookmarkRepository.java
@@ -1,5 +1,7 @@
 package com.showbook.back.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,5 @@ import com.showbook.back.entity.Member;
 
 @Repository
 public interface BookmarkRepository  extends JpaRepository<Bookmark, Long> {
-	Bookmark findByBookAndMember(Book book, Member member);
+	Optional<Bookmark> findByBookAndMember(Book book, Member member);
 }
