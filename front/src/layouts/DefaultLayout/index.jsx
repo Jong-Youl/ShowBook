4af1ccf5714b29f24ba97ff05d7f4278f1b4ce01
허준호 <nidolight@icloud.com>
2024-04-03 11:34:37 +0900

    :lipstick: Design: pwa layout fix

--- front/src/layouts/DefaultLayout/index.jsx
+++ front/src/layouts/DefaultLayout/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router';
 import styled from 'styled-components';
 
 export function MobileLayout() {
+  useEffect(() => {
+    const setAppHeight = () => {
+      const appHeight = window.innerHeight * 0.9;
+      const doc = document.documentElement;
+      doc.style.setProperty('--app-height', `${appHeight}px`);
+    };
+    window.addEventListener('resize', setAppHeight);
+    setAppHeight();
+
+    return () => window.removeEventListener('resize', setAppHeight);
+  }, []);
+
   return (
     <DefaultLayout>
       <Outlet />
@@ -12,9 +24,7 @@ export function MobileLayout() {
 
 const DefaultLayout = styled.div`
   width: 100%;
-  height: -webkit-fill-available;
-  height: fill-available;
-  min-height: 90svh;
+  height: var(--app-height, 100%);
   max-width: 456px;
   margin: 0 auto;
   padding-top: 30px;
