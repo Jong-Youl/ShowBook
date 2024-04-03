e180449a7695f5f14e6af46480a755334a708989
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-04-04 08:19:18 +0900

    :zap: Update: 로그인 이후 loading 화면 위치 수정

--- front/src/pages/MainPage/MainPage.styles.js
+++ front/src/pages/MainPage/MainPage.styles.js
@@ -32,3 +32,13 @@ export const Loading = styled.span`
   font-size: 16px;
   padding: 5px;
 `;
+
+export const LoadingContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+text-align: center;
+height: 100%;
+margin-top:40%;
+`;
