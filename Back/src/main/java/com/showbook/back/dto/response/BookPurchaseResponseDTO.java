2b29482000f706fdd6bbb1363b79cb8e207571a6
Seung Ho Jang <31498000+jshEIT@users.noreply.github.com>
2024-05-21 14:52:33 +0900

    Delete Back directory

--- Back/src/main/java/com/showbook/back/dto/response/BookPurchaseResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package com.showbook.back.dto.response;
-
-import lombok.Builder;
-public record BookPurchaseResponseDTO(String url) {
-	@Builder
-	public BookPurchaseResponseDTO {
-	}
-}
--- Back/src/main/java/com/showbook/back/repository/MemberImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.showbook.back.repository;
-
-import com.showbook.back.entity.MemberImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberImageRepository extends JpaRepository<MemberImage, Long> {
-}
